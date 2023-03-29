fc173d7fdd37fe9570942adc249a89c867b74651
gachr88 <gachr88@users.noreply.github.com>
2023-03-28 18:11:08 -0600

    Update main from server project to enable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.enableCors();
   await app.listen(configService.get('SERVER_PORT'));
 }
 bootstrap();
