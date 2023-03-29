e750303fd86223d7ff2383d7f5ddfbfc75eee7f3
gachr88 <gachr88@users.noreply.github.com>
2023-03-28 18:10:07 -0600

    Update Header component to add data from API

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,14 +1,15 @@
-const Header = () => {
+const Header = ({ data }) => {
+    const { name, owner, lastUpdate,url } = data;
+
     return (
         <header className="border-b-2 border-solid border-black p-2 mb-2">
             <div className="flex">
-                <span className="p-5 m-2 border-solid border-2 rounded-md bg-gray-200 text-xl">R</span>
+                <span className="p-5 m-2 border-solid border-2 rounded-md bg-gray-200 text-xl">{name?.toUpperCase()[0]}</span>
                 <div>
-                    <h1 className="text-xl">Repository Name - Commits History</h1>
-                    <p className="text-lg">Repository owner name</p> 
+                    <h1 className="text-xl">Commits History</h1>
+                    <p className="text-lg text-blue-600"><a href={url} target="_blank"   rel="noreferrer">{owner}/{name}</a></p>
                     <div className="text-sm flex">
-                        <span className="flex-1">Last Update</span>
-                        <span className="flex-1">Count commits</span>
+                        <span className="flex-1">Last Update: {lastUpdate}</span>                        
                     </div>
                 </div>                             
             </div>                                  
